5a534cc2add8d3799f333e1c4e4df8bf4b08deba
Sealgt <silascain6@gmail.com>
2026-01-16 22:13:42 -0500

    Update EPIK.bat

--- EPIK.bat
+++ EPIK.bat
@@ -1,56 +1,83 @@
 @echo off
-title woahwoahwoah
-color 0A
+title Windows Defender - Critical Alert
+color 0C
+mode con cols=90 lines=30
 
-:loop
-set /a r=%random% %% 8
+echo Initializing Windows Security Services...
+ping localhost -n 2 >nul
 
-if %r%==0 goto msg
-if %r%==1 goto title
-if %r%==2 goto beep
-if %r%==3 goto calc
-if %r%==4 goto fakehack
-if %r%==5 goto wait
-if %r%==6 goto colors
-if %r%==7 goto echojunk
-
-:msg
+cls
+echo ==========================================================
+echo   WINDOWS DEFENDER SECURITY ALERT
+echo ==========================================================
 echo.
-echo [RANDOM] Your computer feels funny today.
-goto cont
-
-:title
-title %random%-%random%-%random%
-goto cont
-
-:beep
-echo ^G
-goto cont
-
-:calc
-start calc
-goto cont
-
-:fakehack
-echo INITIALIZING MAINFRAME...
+echo Threats Detected: 7
+echo Severity Level: CRITICAL
+echo.
 ping localhost -n 2 >nul
-echo BYPASSING SECURITY...
+
+echo [!] Trojan.Win32.EPIKSEAL detected
+ping localhost -n 1 >nul
+echo [!] Keylogger.Generic detected
+ping localhost -n 1 >nul
+echo [!] Rootkit.HiddenProcess detected
+ping localhost -n 1 >nul
+echo [!] Memory Injection detected
+ping localhost -n 1 >nul
+echo.
+
+echo Attempting automatic remediation...
+ping localhost -n 3 >nul
+
+cls
+echo Isolating infected memory regions...
 ping localhost -n 2 >nul
-echo ACCESS DENIED LOL
-goto cont
 
-:wait
-ping localhost -n %random% >nul
-goto cont
-
-:colors
-color %random%
-goto cont
-
-:echojunk
-echo %random%%random%%random%%random%%random%
-goto cont
-
-:cont
+echo Overwriting compromised system buffers...
 ping localhost -n 2 >nul
-goto loop
+
+echo.
+echo ERROR: Access denied.
+ping localhost -n 2 >nul
+
+echo.
+echo !!! SYSTEM BREACH CONFIRMED !!!
+ping localhost -n 2 >nul
+
+cls
+color 4F
+echo **************************************************
+echo *  CRITICAL SYSTEM FAILURE                      *
+echo *  UNAUTHORIZED REMOTE ACCESS DETECTED          *
+echo *  DATA TRANSMISSION IN PROGRESS                *
+echo **************************************************
+echo.
+ping localhost -n 2 >nul
+
+echo Uploading system information...
+ping localhost -n 2 >nul
+echo Uploading saved credentials...
+ping localhost -n 2 >nul
+echo Uploading browser data...
+ping localhost -n 2 >nul
+
+cls
+echo FINAL WARNING
+echo.
+echo DO NOT TURN OFF YOUR COMPUTER
+echo DO NOT CLOSE THIS WINDOW
+echo.
+echo Manual intervention required.
+echo.
+echo Contact system administrator immediately.
+echo.
+pause >nul
+
+cls
+color 0A
+echo Just kidding.
+echo.
+echo This was a FAKE virus.
+echo Your system is fine :)
+echo.
+pause
